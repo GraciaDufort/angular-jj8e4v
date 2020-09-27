f054f871fb9ac4e0702104ba1fae6939fb02ee15
GraciaDufort <graciadufort@gmail.com>
2020-09-27 18:15:26 -0400

    Added notification for 'Notify Me' button

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Input } from '@angular/core'
+import { Input } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-product-alerts',
@@ -7,8 +8,9 @@ import { Input } from '@angular/core'
   styleUrls: ['./product-alerts.component.css']
 })
 export class ProductAlertsComponent implements OnInit {
-  
+
   @Input() product;
+  @Output() notify = new EventEmitter();
   constructor() { }
 
   ngOnInit() {
