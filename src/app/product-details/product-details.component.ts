b51933be0357391e5f33a39562f8016be5fb5634
GraciaDufort <graciadufort@gmail.com>
2020-09-27 19:25:30 -0400

    Added CartService usage to product-details component

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import { products } from '../products'
+import { products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -14,12 +15,18 @@ export class ProductDetailsComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
+    private cartService: CartService
   ) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       this.product = products[+params.get('productId')]
     })
+  }
+
+  addToCart(product){
+    this.cartService.addToCart(product);
+    window.alert('Your product has been addded to the cart!');
   }
 
 }
